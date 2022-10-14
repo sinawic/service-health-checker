2b20063e5043261b0fc2a2f6cdf24eb673f5dd0d
sinawic <sina_041_sm@yahoo.com>
2022-10-15 00:35:04 +0330

    some comments

--- tcp.js
+++ tcp.js
@@ -1,21 +1,24 @@
 // Get net module 
 const net = require('net');
-const upstreams = require('./upstream.json')
+const upstreams = require('./upstreams.json')
 
-const INTERVAL = 5000
+const INTERVAL = 5000, TIMEOUT = 2000
 
 console.log(`Started listening to ${upstreams.length} servers...`)
-//Connect to the server on the configured port 
+// Connect to the server on the configured port 
 setInterval(() => {
 
   upstreams.map((upstream, i) => {
     const client = new net.Socket();
-    client.setTimeout(2000, () => {
+
+    client.setTimeout(TIMEOUT, () => {
+      // Logs when could not make connection within X secs
       console.log(`${i + 1}. ${upstream.name}:\t Connection timedout \t ${upstream.host}:${upstream.port}`)
       client.destroy()
     })
+
     client.connect(upstream.port, upstream.host, () => {
-      //Log when the connection is established
+      // Logs when the connection is established
       console.log(`${i + 1}. ${upstream.name}:\t Connected \t\t ${upstream.host}:${upstream.port}`);
       client.destroy()
     });
